e07462980e8adf9301daef24ad7474aa40718ee3
Preenon Chisty <55503621+Preenon@users.noreply.github.com>
2024-03-23 19:47:32 -0400

    Changes to accomodate TDD-net dataset

--- DetectPCBDefectsUsingYOLOXNetwork.m
+++ DetectPCBDefectsUsingYOLOXNetwork.m
@@ -1,14 +1,13 @@
 %% Download pretrained YOLOX detector
-trainedPCBDefectDetectorNet_url = "https://ssd.mathworks.com/supportfiles/"+ ...
-    "vision/data/trainedPCBDefectDetectorYOLOX.zip";
-downloadTrainedNetwork(trainedPCBDefectDetectorNet_url,pwd);
+%trainedPCBDefectDetectorNet_url = "https://ssd.mathworks.com/supportfiles/"+ ...
+ %   "vision/data/trainedPCBDefectDetectorYOLOX.zip";
+%downloadTrainedNetwork(trainedPCBDefectDetectorNet_url,pwd);
 load("trainedPCBDefectDetectorYOLOX.mat");
 
 %% Example detection and classification on one image
-sampleImage = imread(fullfile("DeepPCB-master", "PCBData", ...
-    "group00041", "00041", ...
-    "00041000_test.jpg"));
-sampleImage = cat(3, sampleImage, sampleImage, sampleImage);
+sampleImage = imread(fullfile("pcb_defect_detection_v2i_voc", "images", ...
+    "01_missing_hole_01_jpg.rf.03d45aa38c40a08fbc3a01bfb1dfa434.jpg"));
+%sampleImage = cat(3, sampleImage, sampleImage, sampleImage);
 imshow(sampleImage);
 [bboxes,scores,labels] = detect(detector,sampleImage);
 title("Predicted Defects");
@@ -18,12 +17,12 @@ exportgraphics(ax, "YOLOX predictions.jpg");
 
 %% Prepare data for (hypothetical) training
 % Create an image datastore that reads and manages the image data.
-imageDir = fullfile("PCB_DATASET","images");
+imageDir = fullfile("pcb_defect_detection_v2i_voc","images");
 imds = imageDatastore(imageDir,FileExtensions=".jpg",IncludeSubfolders=true);
 %Create a file datastore that reads the annotation data from XML files. 
 % Specify a custom read function that parses the XML files and extracts the bounding box information. 
 % The custom read function, readPCBDefectAnnotations, is attached to the example as a supporting file.
-annoDir = fullfile("PCB_DATASET","Annotations");
+annoDir = fullfile("pcb_defect_detection_v2i_voc","annotations");
 fds = fileDatastore(annoDir,ReadFcn=@readPCBDefectAnnotations, ...
     FileExtensions=".xml",IncludeSubfolders=true);
 % Save the labeled bounding box data as a box label datastore.
@@ -79,9 +78,9 @@ writetable(averagePrecisionTable, "AveragePrecisionTable.csv");
 % The ideal precision is 1 at all recall levels. Plot the PR curve for the test data.
 class = 1;
 plot(recall{class},precision{class});
-title(sprintf("Average Precision for '" + classNames(class) + "' Defect: " + "%.2f",averagePrecision(class)));
 xlabel("Recall");
 ylabel("Precision");
+title(sprintf("Average Precision for '" + classNames(class) + "' Defect: " + "%.2f",averagePrecision(class)));
 grid on;
 ax = gca;
 exportgraphics(ax, "Average Precision for " + classNames(class) + ".jpg");
@@ -100,9 +99,9 @@ boxes = objectLabels(:,1);
 boxes = vertcat(boxes{:});
 boxArea = prod(boxes(:,3:4),2);
 histogram(boxArea);
-title("Bounding Box Area Distribution");
 xlabel("Box Area");
 ylabel("Count");
+title("Bounding Box Area Distribution");
 ax = gca;
 exportgraphics(ax, "Bounding Box Area Distribution.jpg");
 
